831e2ae30ad0817f4da1e3c694ea309540005c3d
Trey <macbook@macbooks-macbook-pro-2.local>
2018-05-24 07:31:44 -0700

    first successful build

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        print(buttonCount)
+        if buttonCount >= 10 {view.backgroundColor = UIColor.red
+ myLabel.text = "Whats Up" }
+          }
     override func viewDidLoad() {
+      
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+  
     }
-
+   
+    
     override func didReceiveMemoryWarning() {
+     
         super.didReceiveMemoryWarning()
+      
+        
+        
         // Dispose of any resources that can be recreated.
     }
 
